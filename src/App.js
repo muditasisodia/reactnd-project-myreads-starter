2a59cac5cc1f11435ee43c68c859f08d91f7d655
muditasisodia <muditasisodia@gmail.com>
2018-11-23 18:00:45 +0530

    Move Search to new component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link, Route } from 'react-router-dom';
 import Shelf from './components/Shelf';
+import Search from './components/Search';
 import * as BooksAPI from './BooksAPI'
 import './App.css'
 
@@ -37,26 +38,7 @@ class BooksApp extends React.Component {
     return (
       <div className="app">
         <Route exact path="/search" render={()=> (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <Link className="close-search" to = "/">Close</Link>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <Search />
         )} />
 
         <Route exact path = "/" render={()=>(
